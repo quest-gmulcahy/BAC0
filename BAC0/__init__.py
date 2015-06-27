5b16262cc65f8f29ce48b110f858849ebe9063ef
Christian Tremblay, ing <christian.tremblay@servisys.com>
2015-06-26 22:10:22 -0400

    Initial commit top gh-pages branch
    Signed-off-by: Christian Tremblay, ing. <christian.tremblay@servisys.com>

--- BAC0/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-from . import core
-from .scripts.BasicScript import BasicScript 
-from .scripts.ReadWriteScript import ReadWriteScript 
--- BAC0/core/functions/__init__.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-from . import discoverPoints
-from . import WhoisIAm
